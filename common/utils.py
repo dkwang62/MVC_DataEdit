10cf722b6c676fe1c1d871c9708cf5d39431735e
dkwang62 <korealocks@gmail.com>
2025-12-11 13:40:13 +0800

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -60,7 +60,7 @@ REGION_FALLBACK = 99
 
 # US state and DC we treat as "USA" region
 US_STATE_CODES = {
-    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA",
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IL", "IN", "IA",
     "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
     "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",
     "VA", "WA", "WV", "WI", "WY", "DC",
